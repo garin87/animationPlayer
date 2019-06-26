8aaaf9933c494cfaf305bb467f068298c4739d1c
garin87 <garin87@tut.by>
2019-06-27 00:35:15 +0300

    fix: fix all bugs and add new features: change fps; full screen animation.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const canvastAnimation = document.getElementById('canvasPlayer');
 const contextAnimation = canvastAnimation.getContext('2d');
 
 
-const clickX = [];
-const clickY = [];
-const clickDrag = [];
+let clickX = [];
+let clickY = [];
+let clickDrag = [];
 let paint;
 
 // //////////////////////// pencil tool
@@ -60,7 +60,6 @@ canvas.addEventListener('mousemove', function (e) {
     redraw();
   }
 });
-
 // //////////////////////// end pencil tool
 // //////////////////// to clear main canvas
 function clearMainCanvas() {
@@ -71,15 +70,15 @@ function clearMainCanvas() {
 }
 document.getElementById('clearCanvas').addEventListener('click', clearMainCanvas);
 // //////////////////// end to clear main canvas
-// ///////////////////////////////////add new frame
+// //////////////////// add new frame options
 const setIsFrames = {};
 let itemCanvas = 1;
-let countFrame = 1;
+let countFrame = 0;
 
-function saveFrame(clickX, clickY, clickDrag) {
-  const tempClickX = Array.from(clickX);
-  const tempClickY = Array.from(clickY);
-  const tempClickDrag = Array.from(clickDrag);
+function saveFrame(valueX, valueY, valueDrag) {
+  const tempClickX = Array.from(valueX);
+  const tempClickY = Array.from(valueY);
+  const tempClickDrag = Array.from(valueDrag);
   const paramFrame = [];
 
   if (paramFrame.length === 0) {
@@ -93,52 +92,72 @@ function saveFrame(clickX, clickY, clickDrag) {
 }
 
 // ////////////////////////////// handle animation
-function renderFrames(clickX, clickY, clickDrag) {
+function renderFrames(contextAnimation, valueX, valueY, valueDrag) {
   contextAnimation.clearRect(0, 0, context.canvas.width, context.canvas.height);
   contextAnimation.strokeStyle = '#ffa116';
   contextAnimation.lineJoin = 'round';
   contextAnimation.lineWidth = 5;
-  for (let i = 0; i < clickX.length; i += 1) {
+  for (let i = 0; i < valueX.length; i += 1) {
     contextAnimation.beginPath();
-    if (clickDrag[i] && i) {
-      contextAnimation.moveTo(clickX[i - 1], clickY[i - 1]);
+    if (valueDrag[i] && i) {
+      contextAnimation.moveTo(valueX[i - 1], valueY[i - 1]);
     } else {
-      contextAnimation.moveTo(clickX[i] - 1, clickY[i]);
+      contextAnimation.moveTo(valueX[i] - 1, valueY[i]);
     }
-    contextAnimation.lineTo(clickX[i], clickY[i]);
+    contextAnimation.lineTo(valueX[i], valueY[i]);
     contextAnimation.closePath();
     contextAnimation.stroke();
   }
 }
 
 let count = 1;
-let handleAnimation;
-function animatFrames(setIsFrames) {
+let animate;
+
+function animateFrames(frames) {
   const arrayFrames = [];
-  Object.keys(setIsFrames).forEach((key) => {
-    arrayFrames.push(setIsFrames[key]);
+
+  Object.keys(frames).forEach((key) => {
+    arrayFrames.push(frames[key]);
   });
+
   function handle() {
+    if (arrayFrames.length === 0) {
+      contextAnimation.clearRect(0, 0, contextAnimation.canvas.width, contextAnimation.canvas.height);
+      return false;
+    }
     const frame = arrayFrames[count % arrayFrames.length];
-    renderFrames(frame[0], frame[1], frame[2]);
+    renderFrames(contextAnimation, frame[0], frame[1], frame[2]);
     count += 1;
   }
-  handleAnimation = setInterval(handle, 1000 / 3);
+  clearInterval(animate);
+  const fps = document.getElementById('fpsInputId').value;
+  animate = setInterval(handle, 1000 / fps);
 }
 
-document.getElementById('start').addEventListener('click', () => {
-  clearInterval(handleAnimation);
-  animatFrames(setIsFrames);
-});
+function startAnimate() {
+  clearInterval(animate);
+  animateFrames(setIsFrames);
+}
 
+document.getElementById('fpsInputId').addEventListener('click', startAnimate);
 
 // /////////////////////////////////////// add new div frame
-function addNewFrame(countFrame) {
+
+function addNewFrame(numberFrame) {
+  function addActive() {
+    const current = document.querySelector('.active');
+    current.className = current.className.replace(' active', ' ');
+  }
+
+  if (document.querySelector('.active') !== null) {
+    addActive();
+  }
+
   const frame = `
-     <div class="frame" id="frame${countFrame}">
-          <canvas id="canvasFrame${countFrame}" width="100" height="100"></canvas>
-          <button id="deleteFrame${countFrame}" class="deleteFrame">Delete</button>
-          <button id="copyFrame${countFrame}" class="copyFrame">Copy</button>
+     <div class="frame active" data-minframe="${numberFrame}" id="frame${numberFrame}" >
+          <canvas data-mincanvas-number="${numberFrame}" id="canvasFrame${numberFrame}" class="min-canvas" width="100" height="100"></canvas>
+          <button data-mincanvas-delete="${numberFrame}" id="deleteFrame${numberFrame}" class="deleteFrame">Delete</button>
+          <button data-mincanvas-copy="${numberFrame}" id="copyFrame${numberFrame}" class="copyFrame">Copy</button>
      </div>
      `;
 
@@ -147,26 +166,57 @@ function addNewFrame(countFrame) {
 
 // /////////////////////////////////////// delete frame
 
-function deleteFrame(setIsFrames, number) {
-  delete setIsFrames[number];
+function deleteFrame(frames, number) {
+  delete frames[number];
 }
+
 function deleteDivFrame(number) {
   document.getElementById(`frame${number}`).remove();
 }
+
 function removeItem(event) {
-  const { target } = event;
-  const { id } = event.target;
-  const strId = id.substr(11);
-  if (id === 'deleteFrame1') return false;
-  if (target === document.getElementById(`deleteFrame${strId}`)) {
-    deleteFrame(setIsFrames, strId);
-    deleteDivFrame(strId);
+  const valueDelete = event.target.getAttribute('data-mincanvas-delete');
+
+  if (valueDelete) {
+    deleteFrame(setIsFrames, valueDelete);
+    deleteDivFrame(valueDelete);
+    startAnimate();
   }
 }
 
 document.getElementById('listFrames').addEventListener('click', removeItem);
 
 // ///////////////////////////////////// copy frame
+function copyCoordinates(valueX, valueY, valueDrag) {
+  const tempClickX = Array.from(valueX);
+  const tempClickY = Array.from(valueY);
+  const tempClickDrag = Array.from(valueDrag);
+  const paramFrame = [];
+
+  if (paramFrame.length === 0) {
+    paramFrame.push(tempClickX);
+    paramFrame.push(tempClickY);
+    paramFrame.push(tempClickDrag);
+  } else {
+    paramFrame.length = 0;
+  }
+  return paramFrame;
+}
+
+function nandlerCopyMinCanvas(copycanvas) {
+  function copyMinCanvas(image) {
+    const activeMinCanvas = document.querySelector('.active > .min-canvas');
+    const contexMin = activeMinCanvas.getContext('2d');
+    let tempImg = image;
+    contexMin.drawImage(tempImg, 0, 0, 100, 100);
+    tempImg = 0;
+  }
+
+  function renderMinScreenshot(activeCanvas) {
+    copyMinCanvas(activeCanvas);
+  }
+  renderMinScreenshot(copycanvas);
+}
 
 function copyFrame(event) {
   const { target } = event;
@@ -175,47 +225,88 @@ function copyFrame(event) {
 
   if (target === document.getElementById(`copyFrame${strId}`)) {
     const copyItem = Object.assign({}, setIsFrames[`${strId}`]);
+    const activeMinCanvas = document.querySelector('.active > .min-canvas');
     countFrame += 1;
     setIsFrames[`${countFrame}`] = copyItem;
     saveFrame(copyItem[0], copyItem[1], copyItem[2]);
     addNewFrame(countFrame);
+    nandlerCopyMinCanvas(activeMinCanvas);
+    startAnimate();
   }
 }
+
 document.getElementById('listFrames').addEventListener('click', copyFrame);
-// /////////////////////////////////  screen copy from base canvas min canvas
-function copyScreenCanvas(event) {
-  const { target } = event;
-  const { id } = event.target;
-  const strId = id.substr(11);
-  function resizeCanvasImg(image, canvasId) {
-    const canvasMin = document.getElementById(`canvasFrame${canvasId}`);
-    const contexMin = canvasMin.getContext('2d');
+
+function nandlerCopyCanvasImg() {
+  function copyCanvasImg(image) {
+    const activeMinCanvas = document.querySelector('.active > .min-canvas');
+    const contexMin = activeMinCanvas.getContext('2d');
     let tempImg = image;
     contexMin.drawImage(tempImg, 0, 0, 100, 100);
     tempImg = 0;
   }
-  function renderScreenshot(strId) {
+
+  function renderScreenshot() {
     const screenShot = document.getElementById('canvas');
-    resizeCanvasImg(screenShot, strId);
+    copyCanvasImg(screenShot);
   }
 
-  if (target === document.getElementById(`canvasFrame${strId}`)) {
-    canvas.addEventListener('mouseup', renderScreenshot(strId));
-  }
-
-  if (target === document.getElementById('newframe')) {
-    const temp = Object.keys(setIsFrames).length;
-    canvas.addEventListener('mouseup', renderScreenshot(temp + 1));
-  }
+  renderScreenshot();
 }
 
+function copyMinCanvasImg() {
+  const activeMainCanvas = document.querySelector('.active > .min-canvas');
+  const numberMinCanvas = activeMainCanvas.getAttribute('data-mincanvas-number');
+  const coordinates = setIsFrames[numberMinCanvas];
+  clickX = coordinates[0];
+  clickY = coordinates[1];
+  clickDrag = coordinates[2];
 
-document.getElementById('listFrames').addEventListener('click', copyScreenCanvas);
-document.getElementById('newframe').addEventListener('click', copyScreenCanvas);
+  renderFrames(context, coordinates[0], coordinates[1], coordinates[2]);
+}
 
+document.getElementById('wrapperListFrame').addEventListener('click', (event) => {
+  const minFrame = event.target.getAttribute('data-minframe');
+  const minCanvas = event.target.getAttribute('data-mincanvas-number');
+  const listClassframe = document.querySelectorAll('.frame');
+  const copyMinCanvas = event.target.getAttribute('data-mincanvas-copy');
+
+  if (minFrame || minCanvas) {
+    listClassframe.forEach((activeclass) => {
+      activeclass.classList.remove('active');
+    });
+    event.target.classList.add('active');
+    copyMinCanvasImg();
+  }
+  if (copyMinCanvas) {
+    const activeFrame = document.querySelector(`#frame${copyMinCanvas}`);
+    listClassframe.forEach((activeclass) => {
+      activeclass.classList.remove('active');
+    });
+    activeFrame.classList.add('active');
+    copyMinCanvasImg();
+  }
+});
+
+// /////////////////////////// handle add new frame
 document.getElementById('newframe').addEventListener('click', () => {
   countFrame += 1;
   saveFrame(clickX, clickY, clickDrag);
-  clearMainCanvas();
   addNewFrame(countFrame);
+  nandlerCopyCanvasImg();
+  startAnimate();
 });
+
+// /////////////////////////////////  full screen animation
+function fullScreen() {
+  const canvasAnimation = document.getElementById('canvasPlayer');
+  if (!document.fullscreenElement) {
+    canvasAnimation.requestFullscreen().catch((err) => {
+      console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+    });
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+document.getElementById('fullScreenAnimation').addEventListener('click', fullScreen);
